490b1052ae2c44e7d83404fa5c3d2c17014c5139
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-05-24 20:23:28 +0100

    Refactor LinkedList class and add visualisation feature

--- main.py
+++ main.py
@@ -4,63 +4,59 @@ class Node:
         self.next = next
 
 class LinkedList:
-    class LinkedList:
-        def __init__(self):
-            # Initialize an empty linked list with no head node
-            self.head = None
+    def __init__(self):
+        self.head = None
 
-        def append(self, data):
-            # If the linked list is empty, create a new node and set it as the head
-            if not self.head:
-                self.head = Node(data)
-
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
             current = self.head
-            # Traverse to the last node
             while current.next:
                 current = current.next
-            # Create a new node and set it as the next node of the last node
             current.next = Node(data)
-        
 
-        def print_list(self):
-            current = self.head
-            # singly linked lists start with the head node
-            while current:
-                print(current.data)
-                current = current.next
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+    def insert(self, data, index):
+        if index == 0:
+            self.head = Node(data, self.head)
+            return
+
+        current = self.head
+        for i in range(index - 1):
+            if not current:
+                raise IndexError("Index out of range")
+            current = current.next
+        current.next = Node(data, current.next)
+
+    def delete(self, index):
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        for i in range(index - 1):
+            if not current:
+                raise IndexError("Index out of range")
+            current = current.next
+        current.next = current.next.next
 
 
-        def insert(self, data, index):
-            # If the index is 0, insert the new node at the head
-            if index == 0:
-                self.head = Node(data, self.head)
-                return
+class visualiser:
+    def __init__(self):
+        pass
 
-            current = self.head
-            # Traverse to the node before the index
-            for i in range(index - 1):
-                if not current:
-                    raise IndexError("Index out of range")
-                current = current.next
-            # Create a new node and set it as the next node of the current node
-            current.next = Node(data, current.next)
-
-
-        def delete(self, index):
-            # If the index is 0, delete the head node
-            if index == 0:
-                self.head = self.head.next
-                return
-
-            current = self.head
-            # Traverse to the node before the index
-            for i in range(index - 1):
-                if not current:
-                    raise IndexError("Index out of range")
-                current = current.next
-            # Set the next node of the current node to the next node of the next node
-            current.next = current.next.next
-
+    def visualise(self, linked_list):
+        current = linked_list.head
+        while current:
+            print(f"{current.data} -> ", end="")
+            current = current.next
+        print("None")
 
 if __name__ == "__main__":
     linked_list = LinkedList()
@@ -70,5 +66,9 @@ if __name__ == "__main__":
     linked_list.print_list()
     linked_list.insert(4, 1)
     linked_list.print_list()
-    linked_list.delete(1)
+    linked_list.delete(2)
     linked_list.print_list()
+    print('\n')
+    print("Visualising the linked list: ")
+    visualiser = visualiser()
+    visualiser.visualise(linked_list)
